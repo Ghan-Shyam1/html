f7845ec55c1c95571aba2aaa3a9dec67400d0445
Ghan-Shyam1 <ghanshyammahato697@gmail.com>
2025-01-21 17:24:30 +0545

    first page

---
+++ my-app/src/App.js
@@ -0,0 +1,28 @@
+  
+  
+  
+ import React from 'react';
+
+ function App() {
+   return (
+     <div style={{ textAlign: "center", padding: "50px" }}>
+       <h1>Welcome to My First React App!</h1>
+       <p>This is my first React project. 🎉</p>
+     </div>
+   );
+ }
+ 
+ export default App;
+
+ function Video({ video }) {
+  return (
+    <div>
+      <Thumbnail video={video} />
+      <a href={video.url}>
+        <h3>{video.title}</h3>
+        <p>{video.description}</p>
+      </a>
+      <LikeButton video={video} />
+    </div>
+  );
+}
